a7d9a6835a1ef6d4bf633666481aba0b7934c638
Aravind Vattappallil <aravindvr99@gmail.com>
2023-02-12 16:19:02 -0500

    added more genre to home

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./Components/NavBar/NavBar";
 import "./App.css"
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
-import { action,orginals } from "./urls";
+import { action,comedy,documentaries,horror,orginals, romance, trending } from "./urls";
 
 
 function App() {
@@ -12,7 +12,13 @@ function App() {
      // <NavBar />
       <Banner />
       <RowPost url = {orginals} title ="Netflix Orginals"/>
+      <RowPost url={trending} title="Trending" isSmall />
       <RowPost url={action} title="Action" isSmall />
+      <RowPost url={comedy} title="Comedy" isSmall />
+      <RowPost url={horror} title="Horror" isSmall />
+      <RowPost url={romance} title="Romance" isSmall />
+      <RowPost url={documentaries} title="Documentaries" isSmall />
+
 
     </div>
   );
